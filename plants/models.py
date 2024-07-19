bfca97502c086dadeaee7e2e8060fbde9daa546b
daniegitx1 <mailbox.2332@proton.me>
2024-07-19 16:53:27 +0200

    First Commit from Server to GIT

--- plants/models.py
+++ plants/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class PlantsData(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, default='some plant')
     picture = models.URLField(max_length=200)
+    picture2 = models.URLField(max_length=200)
 
     def __str__(self):
         return str(self.name)
